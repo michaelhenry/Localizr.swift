2a3631451154df8d28ad19279d31f701c1651e38
Michael Henry Pantaleon <me@iamkel.net>
2018-05-29 19:02:32 +0900

    initial commit of Localizr_iOS

--- Localizr-iOS.podspec
+++ Localizr-iOS.podspec
@@ -8,35 +8,22 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Localizr-iOS'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of Localizr-iOS.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
+  s.version          = ENV["RXRETROFIT_VERSION"] || '1.0.0'
+  s.summary          = 'An iOS library that simplies the Localization process.'
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+An iOS library that simplies the Localization process. 
+It\'s recommended to use Localizr server (https://github.com/michaelhenry/Localizr) so that the localization process can be automated and can be done by different translators.
                        DESC
 
-  s.homepage         = 'https://github.com/Michael Henry Pantaleon/Localizr-iOS'
+  s.homepage         = 'https://github.com/michaelhenry/Localizr-iOS'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Michael Henry Pantaleon' => 'me@iamkel.net' }
-  s.source           = { :git => 'https://github.com/Michael Henry Pantaleon/Localizr-iOS.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/michaelhenry/Localizr-iOS.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
-
+  s.ios.deployment_target = '9.0'
   s.source_files = 'Localizr-iOS/Classes/**/*'
-  
-  # s.resource_bundles = {
-  #   'Localizr-iOS' => ['Localizr-iOS/Assets/*.png']
-  # }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.swift_version = '4.1'
 end
---
+++ Localizr-iOS/Classes/Localizr.swift
@@ -0,0 +1,82 @@
+//
+//  Localizr.swift
+//  FBSnapshotTestCase
+//
+//  Created by Michael Henry Pantaleon on 2018/05/29.
+//
+
+import Foundation
+
+enum LocalizrKey:String {
+  
+  case appleLocale    = "AppleLocale"
+  case currentLocale  = "Localizr_current_locale"
+}
+
+public class Localizr {
+  
+  /**
+   Retrieve the current system locale.
+  */
+  public static func systemLocale() -> String? {
+    return UserDefaults.standard.string(forKey: LocalizrKey.appleLocale.rawValue)
+  }
+  
+  /**
+   List of supported locales
+ 
+   - Parameters:
+      - excludeBase: default value is true
+  */
+  public static func supportedLocales(_ excludeBase:Bool = true) -> [String] {
+    var _localizations = Bundle.main.localizations
+    if let indexOfBase = _localizations.index(of: "Base"), excludeBase == true {
+      _localizations.remove(at: indexOfBase)
+    }
+    return _localizations
+  }
+  
+  /**
+   Update the current locale
+  */
+  public static func update(locale:String) {
+    if Localizr.supportedLocales().contains(locale) {
+      UserDefaults.standard.set(locale, forKey: LocalizrKey.currentLocale.rawValue)
+    }
+  }
+  
+  /**
+   To get current Locale.
+  */
+  public static func currentLocale() -> String? {
+    return UserDefaults.standard.string(forKey: LocalizrKey.currentLocale.rawValue) ?? Localizr.systemLocale()
+  }
+}
+
+extension String {
+  
+  /**
+   An extension method of a string to retrieve the localized value/text.
+   
+   - Parameters:
+      - repeated: (Optional) This is the number of count where the text will going to repeat. It's good for debugging the size of of UILabels assuming that in other languages, some phrases will be 2 or 3 times larger than in english.
+ 
+  */
+  public func localized(repeated:Int = 1) -> String? {
+    
+    let localeCode = UserDefaults.standard.string(forKey: LocalizrKey.currentLocale.rawValue)
+    var s = ""
+    if let path = Bundle.main.path(forResource: localeCode, ofType: "lproj") {
+      if let bundle = Bundle(path: path) {
+        s =  NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
+      }
+    } else {
+      s = NSLocalizedString(self, comment: "")
+    }
+    
+    if repeated > 1 {
+      return String(repeating: s, count: repeated)
+    }
+    return s
+  }
+}
