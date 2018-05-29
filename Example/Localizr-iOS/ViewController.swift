15a6fed36aaf7dbb0fe181e69b1de1264efcb4b2
Michael Henry Pantaleon <me@iamkel.net>
2018-05-29 19:12:38 +0900

    Update sample project

--- Example/Localizr-iOS/ViewController.swift
+++ Example/Localizr-iOS/ViewController.swift
@@ -7,18 +7,99 @@
 //
 
 import UIKit
+import Localizr_iOS
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UITableViewController {
+  
+  lazy var keys = [
+    "ALWAYS",
+    "BACK",
+    "CANCEL",
+    "DONE",
+    "DOWN",
+    "FORWARD",
+    "GOOD_AFTERNOON",
+    "GOOD_BYE",
+    "GOOD_LUCK",
+    "GOOD_MORNING",
+    "GOOD_NIGHT",
+    "GREAT",
+    "GREETING",
+    "GTG",
+    "HELLO",
+    "HOME",
+    "HOW_ARE_YOU",
+    "I_HATE_YOU",
+    "I_LOVE_YOU",
+    "I_MISS_YOU",
+    "INFO",
+    "LEFT",
+    "NAME",
+    "NICE_TRY",
+    "OK",
+    "OMG",
+    "RIGHT",
+    "SAVE",
+    "SEE_YOU",
+    "SUCCESSFUL",
+    "TAKE_CARE",
+    "UP",
+    "WARNING"
+  ]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 100
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return keys.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let localeCell = tableView.dequeueReusableCell(withIdentifier: "locale_cell")!
+    localeCell.textLabel?.text = keys[indexPath.row]
+    localeCell.detailTextLabel?.text = keys[indexPath.row].localized()
+    return localeCell
+  }
+  
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Current Locale: " + (
+      Localizr.currentLocale()?.localized() ??
+        Localizr.systemLocale()?.localized() ?? "unknown")
+  }
+  
+  @IBAction func changeLocale(_ sender: Any) {
+    let localeSelector = UIAlertController(title: "Select a Language", message: nil, preferredStyle: .actionSheet)
+    
+    Localizr.supportedLocales().forEach { localeCode in
+      
+      localeSelector.addAction(UIAlertAction(title: localeCode, style: .default, handler: { (action) in
+        Localizr.update(locale: localeCode)
+        self.tableView.reloadData()
+      }))
+      
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    localeSelector.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (action) in
+      
+    }))
+    
+    present(localeSelector, animated: true) {
+      
     }
-
+  }
 }
+
 
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,49 +2,51 @@
 
 import Quick
 import Nimble
-import Localizr-iOS
+import Localizr_iOS
 
+// TODO: Update test cases
 class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
+  override func spec() {
+    describe("these will fail") {
+      
+      it("can do maths") {
+        expect(1) == 1
+      }
+      
+      it("can read") {
+        expect("number") == "number"
+      }
+      
+      it("will eventually fail") {
+        expect("time").toEventually( equal("time") )
+      }
+      
+      context("these will pass") {
+        
+        it("can do maths") {
+          expect(23) == 23
         }
+        
+        it("can read") {
+          expect("🐮") == "🐮"
+        }
+        
+        it("will eventually pass") {
+          var time = "passing"
+          
+          DispatchQueue.main.async {
+            time = "done"
+          }
+          
+          waitUntil { done in
+            Thread.sleep(forTimeInterval: 0.5)
+            expect(time) == "done"
+            
+            done()
+          }
+        }
+      }
     }
+  }
 }
+
