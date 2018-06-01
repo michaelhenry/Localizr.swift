35ffac56448c86efe9309f60f11b79e81f0f3315
Michael Henry Pantaleon <me@iamkel.net>
2018-06-01 10:20:12 +0900

    update comments [ci skip]

--- Localizr.swift/Classes/Localizr.swift
+++ Localizr.swift/Classes/Localizr.swift
@@ -2,25 +2,25 @@ import Foundation
 
 enum LocalizrKey:String {
   
+  /// Apple's Locale key
   case appleLocale    = "AppleLocale"
+  /// Localizr Current Locale key
   case currentLocale  = "Localizr_current_locale"
 }
 
 public class Localizr {
   
-  /**
-   Retrieve the current system locale.
-  */
+  /// Retrieve the current system Locale
+  ///
+  /// - Returns: String?
   public static func systemLocale() -> String? {
     return UserDefaults.standard.string(forKey: LocalizrKey.appleLocale.rawValue)
   }
   
-  /**
-   List of supported locales
- 
-   - Parameters:
-      - excludeBase: default value is true
-  */
+  /// List of supported locales
+  ///
+  /// - Parameter excludeBase: (default value: true)
+  /// - Returns: [String]
   public static func supportedLocales(_ excludeBase:Bool = true) -> [String] {
     var _localizations = Bundle.main.localizations
     if let indexOfBase = _localizations.index(of: "Base"), excludeBase == true {
@@ -29,18 +29,18 @@ public class Localizr {
     return _localizations
   }
   
-  /**
-   Update the current locale
-  */
+  /// Set the current locale
+  ///
+  /// - Parameter locale: String
   public static func update(locale:String) {
     if Localizr.supportedLocales().contains(locale) {
       UserDefaults.standard.set(locale, forKey: LocalizrKey.currentLocale.rawValue)
     }
   }
   
-  /**
-   To get current Locale.
-  */
+  /// Get the current locale
+  ///
+  /// - Returns: String?
   public static func currentLocale() -> String? {
     return UserDefaults.standard.string(forKey: LocalizrKey.currentLocale.rawValue) ?? Localizr.systemLocale()
   }
@@ -48,13 +48,10 @@ public class Localizr {
 
 extension String {
   
-  /**
-   An extension method of a string to retrieve the localized value/text.
-   
-   - Parameters:
-      - repeated: (Optional) This is the number of count where the text will going to repeat. It's good for debugging the size of of UILabels assuming that in other languages, some phrases will be 2 or 3 times larger than in english.
- 
-  */
+  /// An extension method of a string to retrieve the localized value/text.
+  ///
+  /// - Parameter repeated: (Optional) This is the number of count where the localized text will going to repeat.
+  /// - Returns: String?
   public func localized(repeated:Int = 1) -> String? {
     
     let localeCode = UserDefaults.standard.string(forKey: LocalizrKey.currentLocale.rawValue)
